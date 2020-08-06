18f134f079d1f8eb91c455697a034ac4ac444d07
2443 <condol1209@gmail.com>
2020-08-07 00:01:23 +0900

    5-11. 쿠키_세션과 전체 로그인 흐름

--- back/app.js
+++ back/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const dotenv = require('dotenv');
 
 const db = require('./models');
 const passportConfig = require('./passport');
@@ -7,7 +11,8 @@ const passportConfig = require('./passport');
 const app = express();
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
-const passport = require('./passport');
+
+dotenv.config();
 
 db.sequelize
   .sync()
@@ -26,6 +31,16 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/post', postRouter);
 app.use('/user', userRouter);
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
+
 const { User } = require('../models');
 
 const router = express.Router();
@@ -19,7 +20,7 @@ router.post('/login', (req, res, next) => {
         console.error(loginErr);
         return next(loginErr);
       }
-      return res.json(user);
+      return res.status(200).json(user);
     });
   })(req, res, next);
 });
@@ -50,6 +51,12 @@ router.post('/', async (req, res, next) => {
     console.log(err);
     next(err);
   }
+});
+
+router.post('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send('ok');
 });
 
 module.exports = router;

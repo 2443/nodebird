0586ff35eef3f4f693820abdd49f5b22e916f114
2443 <condol1209@gmail.com>
2020-08-17 19:58:54 +0900

    fix toBuffer error

--- lambda/index.js
+++ lambda/index.js
@@ -4,17 +4,21 @@ const sharp = require('sharp');
 const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name;
-  const Key = event.Records[0].s3.object.key;
-
+  const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
+  const Key = event.Records[0].s3.object.key; // original/12312312_abc.png
+  console.log(Bucket, Key);
   const filename = Key.split('/')[Key.split('/').length - 1];
   const ext = Key.split('.')[Key.split('.').length - 1];
   const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
+  console.log('filename', filename, 'ext', ext);
+
   try {
     const s3Object = await s3.getObject({ Bucket, Key }).promise();
+    console.log('original', s3Object.Body.length);
     const resizedImage = await sharp(s3Object.Body)
       .resize(400, 400, { fit: 'inside' })
-      .toFormat(requiredFormat).toBuffer;
+      .toFormat(requiredFormat)
+      .toBuffer();
     await s3
       .putObject({
         Bucket,
@@ -22,6 +26,7 @@ exports.handler = async (event, context, callback) => {
         Body: resizedImage,
       })
       .promise();
+    console.log('put', resizedImage.length);
     return callback(null, `thumb/${filename}`);
   } catch (error) {
     console.error(error);
